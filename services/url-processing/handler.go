2a6eceed3f5f48abbbf66480e83fc60c95c7fb67
Dennis Bejze <dennis.bejze@outlook.de>
2022-03-26 22:39:02 +0100

    Fix test

--- services/url-processing/handler.go
+++ services/url-processing/handler.go
@@ -6,16 +6,20 @@ import (
 	"net/http"
 )
 
+type ProcessingRequest struct {
+	LongURL string `json:"long_url"`
+}
+
 func Handle(rw http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodPost:
-		var longURL string
-		if err := json.NewDecoder(r.Body).Decode(&longURL); err != nil {
+		var procRequest ProcessingRequest
+		if err := json.NewDecoder(r.Body).Decode(&procRequest); err != nil {
 			http.Error(rw, "short URL is missing", http.StatusBadRequest)
 			return
 		}
 
-		fmt.Fprint(rw, longURL)
+		fmt.Fprint(rw, procRequest.LongURL)
 
 	default:
 		http.Error(rw, "method is not allowed", http.StatusMethodNotAllowed)
--- services/url-processing/handler_test.go
+++ services/url-processing/handler_test.go
@@ -1,19 +1,32 @@
 package urlprocessing
 
 import (
+	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
 )
 
 func Test_Handler(t *testing.T) {
-	longURL := "https://example.com"
-	req := httptest.NewRequest("POST", "/url-processing", strings.NewReader(longURL))
-	res := httptest.NewRecorder()
+	tests := []struct {
+		body string
+		want string
+	}{
+		{body: `{"long_url": "http://example.com"}`, want: "http://example.com"},
+	}
 
-	Handle(res, req)
+	for _, test := range tests {
+		req, err := http.NewRequest("POST", "/", strings.NewReader(test.body))
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	if shortURL := res.Body.String(); shortURL != longURL {
-		t.Fatalf("expected shortURL %q but got %q", longURL, shortURL)
+		res := httptest.NewRecorder()
+
+		Handle(res, req)
+
+		if shortURL := res.Body.String(); shortURL != test.want {
+			t.Fatalf("expected shortURL %q but got %q", test.want, shortURL)
+		}
 	}
 }
