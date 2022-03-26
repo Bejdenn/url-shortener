31c5c84e33bf454cd1fc0dedf4c5ca8d9c4168b6
Dennis Bejze <dennis.bejze@outlook.de>
2022-03-26 14:02:21 +0100

    US-9 Implement a simple cloud function for testing

---
+++ services/url-processing/deploy-function.sh
@@ -0,0 +1,1 @@
+gcloud functions deploy "url-processing" --entry-point "HandleURLProcessing" --runtime go116 --trigger-http
---
+++ services/url-processing/handler.go
@@ -0,0 +1,10 @@
+package urlprocessing
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func HandleURLProcessing(rw http.ResponseWriter, r *http.Response) {
+	fmt.Fprint(rw, "Hello, world!")
+}
