8f851e68798d09b483a3edbe26289601fed4a831
Dennis Bejze <dennis.bejze@outlook.de>
2022-04-01 12:21:46 +0200

    Prepare handler for debugging

--- functions/short-redirect/handler_test.go
+++ functions/short-redirect/handler_test.go
@@ -1,6 +1,23 @@
 package shortredirect
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/short-redirect/cf687d69", http.NoBody)
+	rec := httptest.NewRecorder()
+
+	Handle(rec, req)
+
+	res := rec.Result()
+
+	if res.StatusCode != http.StatusMovedPermanently {
+		t.Fatalf("got statuscode = %v, want = %v", res.StatusCode, http.StatusMovedPermanently)
+	}
+}
 
 func TestExtractPathParam(t *testing.T) {
 	tests := []string{
