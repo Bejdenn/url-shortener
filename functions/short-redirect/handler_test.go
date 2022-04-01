89b048cdbf7a57e742bc21fd5136b4e0a3145f69
Dennis Bejze <dennis.bejze@outlook.de>
2022-04-01 12:59:18 +0200

    US-15 Fix path param extraction

--- functions/short-redirect/handler_test.go
+++ functions/short-redirect/handler_test.go
@@ -25,7 +25,7 @@ func TestExtractPathParam(t *testing.T) {
 	}
 
 	for _, tc := range tests {
-		if param := extractPathParam("/short-redirect/"+tc, "/short-redirect"); param != tc {
+		if param := extractPathParam(domain + "/short-redirect/" + tc); param != tc {
 			t.Errorf("got = %s, want = %s", param, tc)
 		}
 	}
