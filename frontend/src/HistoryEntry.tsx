94b83e685e9763e17f52ce06adeb4dce2be22680
Dennis Bejze <dennis.bejze@outlook.de>
2022-04-19 19:53:56 +0200

    Truncate long URLs if necessary

--- frontend/src/HistoryEntry.tsx
+++ frontend/src/HistoryEntry.tsx
@@ -1,12 +1,20 @@
 import { Link, Stack, Typography } from '@mui/material';
 import { URLRelation } from './types';
 
+function truncate(value: string, threshold: number) {
+  if (value.length > threshold) {
+    return value.substring(0, threshold) + '...';
+  }
+
+  return value;
+}
+
 const HistoryEntry = ({ relation }: { relation: URLRelation }) => {
   return (
     <Stack direction={{ xs: 'column', sm: 'column', md: 'row' }} margin={2} spacing={2}
            alignItems={{ xs: 'flex-start', sm: 'flex-start', md: 'center' }}
            justifyContent='space-between'>
-      <Typography>{relation.longUrl}</Typography>
+      <Typography>{truncate(relation.longUrl, 65)}</Typography>
       <Link sx={{ fontFamily: 'sans-serif' }} rel='noopener noreferrer' target='_blank'
             href={relation.shortUrl}>{relation.shortUrl}</Link>
     </Stack>
